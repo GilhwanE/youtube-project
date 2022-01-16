d37af3580432f05e99b254256a0e842c06ec6e6f
GilHwan <dyghks123@gmail.com>
2022-01-16 16:10:41 +0900

    💄  Add content moudule and ClassName given

--- src/components/contentMain/contnent.jsx
+++ src/components/contentMain/contnent.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './content.module.css';
 
-const Contnent = (props) => {
-  return <h1>{props.video.snippet.title}</h1>;
-};
+const Contnent = (props) => (
+  <li className={styles.video}>
+    <img
+      className={styles.thumbnails}
+      src={props.video.snippet.thumbnails.medium.url}
+      alt="video thumbnail"
+    />
+    <div className={styles.video_text}>
+      <p className={styles.video_title}>{props.video.snippet.title}</p>
+      <p className={styles.video_subtitle}>
+        {props.video.snippet.channelTitle}
+      </p>
+    </div>
+  </li>
+);
 
+// 각각의 비디오의 타이틀을 호출하는 부분
 export default Contnent;
