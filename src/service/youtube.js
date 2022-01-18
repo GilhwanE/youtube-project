324abb60b5ee7dd5590ae5ea2c04194b299c7d48
GilHwan <dyghks123@gmail.com>
2022-01-18 14:43:54 +0900

    ⚡️  Create class yotube network fetch

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,30 +1,28 @@
 class Youtube {
   constructor(key) {
-    key = this.key;
+    this.key = key;
     this.getRequestOptions = {
       method: 'GET',
       redirect: 'follow',
     };
   }
 
-  mostPopular() {
-    return fetch(
+  async mostPopular() {
+    const response = await fetch(
       `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items); // result에 있는 items를 받아올것   setViedos 업데이트된 부분에 결과값을 받아올것
+    );
+    const result_1 = await response.json();
+    return result_1.items; // result에 있는 items를 받아올것   setViedos 업데이트된 부분에 결과값을 받아올것
   }
 
-  search(query) {
-    return fetch(
+  async search(query) {
+    const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      ); // id에 비디오 아이디로 덮어준다
+    );
+    const result_1 = await response.json();
+    return result_1.items.map((item) => ({ ...item, id: item.id.videoId })); // id에 비디오 아이디로 덮어준다
     // id를 변환해줘야한다 result.itmes에서 가져온 결ㅇ과를
   }
 }
