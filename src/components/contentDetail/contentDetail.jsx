34b6c0b75634033e1169d4b2b145a6f6ac39d1d2
GilHwan <dyghks123@gmail.com>
2022-01-21 17:05:49 +0900

    ✨  Youtube Iframe option add

---
+++ src/components/contentDetail/contentDetail.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './contentDetail.module.css';
+
+const ContentDetail = ({ video, video: { snippet } }) => (
+  //선택된 비디오를 가져오겠지
+
+  <section className={styles.detail}>
+    <iframe
+      className={styles.video}
+      type="text/html"
+      width="100%"
+      height="500px"
+      src={`https://www.youtube.com/embed/${video.id}`}
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
+    <h2>{snippet.title}</h2>
+    <h3>{snippet.channleTitle}</h3>
+    <pre className={styles.desc}>{snippet.description}</pre>
+  </section>
+);
+
+export default ContentDetail;
